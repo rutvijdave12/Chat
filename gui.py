ee0c4a2cc7ec01277e2bd009baa79a3518063dd4
rutvijdave12 <rutvijdave12@gmail.com>
2020-05-07 13:16:04 +0530

    GUI

---
+++ gui.py
@@ -0,0 +1,15 @@
+from tkinter import ttk
+import tkinter as tk
+
+
+
+app = tk.Tk()
+app.iconphoto(False, tk.PhotoImage(file="chat_icon2.png"))
+app.title("Chat Room")
+app.geometry("720x580")
+
+
+app.configure(background='#041c07')
+
+
+app.mainloop()
